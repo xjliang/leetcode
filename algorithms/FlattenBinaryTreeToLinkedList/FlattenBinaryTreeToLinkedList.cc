2c608bd5e6671cbb6780235583c4858662201ada
xjliang <gnulxj@gmail.com>
2021-02-27 09:42:47 +0800

    feat(tree) : FlattenBinaryTreeToLinkedList.cc

---
+++ algorithms/FlattenBinaryTreeToLinkedList/FlattenBinaryTreeToLinkedList.cc
@@ -0,0 +1,68 @@
+// Source : https://leetcode.com/problems/flatten-binary-tree-to-linked-list
+// Author : xjliang
+// Date   : 2021-02-27
+
+/*****************************************************************************************************
+ *
+ * Given the root of a binary tree, flatten the tree into a "linked list":
+ *
+ * 	The "linked list" should use the same TreeNode class where the right child pointer points
+ * to the next node in the list and the left child pointer is always null.
+ * 	The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ *
+ * Example 1:
+ *
+ * Input: root = [1,2,5,3,4,null,6]
+ * Output: [1,null,2,null,3,null,4,null,5,null,6]
+ *
+ * Example 2:
+ *
+ * Input: root = []
+ * Output: []
+ *
+ * Example 3:
+ *
+ * Input: root = [0]
+ * Output: [0]
+ *
+ * Constraints:
+ *
+ * 	The number of nodes in the tree is in the range [0, 2000].
+ * 	-100 <= Node.val <= 100
+ *
+ * Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+ ******************************************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  void flatten(TreeNode* node) {
+    if (node == nullptr) {
+      return ;
+    }
+
+    if (node->right) {
+      flatten(node->right);
+    }
+    if (node->left) {
+      flatten(node->left);
+      TreeNode* rightmost = node->left;
+      while (rightmost && rightmost->right) {
+        rightmost = rightmost->right;
+      }
+      rightmost->right = node->right;
+      node->right = node->left;
+      node->left = nullptr;
+    }
+  }
+};
